5a53882ec5f821d53e4bac2c768667696ba60f60
Jarone Jabonillo <jjabonillo1@student.rccd.edu>
2018-02-28 16:37:17 -0800

    Assignment2_IP
    Signed-off-by: Jarone Jabonillo <jjabonillo1@student.rccd.edu>

---
+++ Homework/Assn2/Assn2_TriangleTable/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=assn2_triangletable
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/assn2_triangletable
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=assn2triangletable.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/assn2triangletable.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=assn2_triangletable
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/assn2_triangletable
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=assn2triangletable.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/assn2triangletable.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
